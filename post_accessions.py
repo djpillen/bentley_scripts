6726d6e5b5a1e5fc9f8f1210b102a854375d418b
djpillen@umich.edu <djpillen@umich.edu>
2015-06-26 13:36:35 -0400

    adding accession scripts

---
+++ post_accessions.py
@@ -0,0 +1,52 @@
+"""
+<--This is an example of a gotten accession with a bunch of stuff-->
+{"lock_version":9,"suppressed":false,"title":"this is the title","display_string":"this is the title","publish":true,"content_description":"This is a content description","condition_description":"This is a condition description","disposition":"THis is the disposition","inventory":"this is the inventory\r\n","provenance":"this is the provenance\r\n","general_note":"this is a general note","accession_date":"2015-01-01","restrictions_apply":true,"retention_rule":"this is the retention rule","access_restrictions":true,"access_restrictions_note":"this is the accessrestriction note","use_restrictions":true,"use_restrictions_note":"this is the use restriction note","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-18T13:21:07Z","system_mtime":"2015-06-19T13:10:59Z","user_mtime":"2015-06-19T13:10:57Z","id_0":"123456","acquisition_type":"deposit","resource_type":"papers","jsonmodel_type":"accession","external_ids":[],"related_accessions":[],"subjects":[{"ref":"/subjects/5"},{"ref":"/subjects/14"},{"ref":"/subjects/1"}],"linked_events":[{"ref":"/repositories/2/events/17"}],"extents":[{"lock_version":0,"number":"5","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-19T13:10:58Z","system_mtime":"2015-06-19T13:10:58Z","user_mtime":"2015-06-19T13:10:58Z","portion":"part","extent_type":"linear_feet","jsonmodel_type":"extent"},{"lock_version":0,"number":"100","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-19T13:10:58Z","system_mtime":"2015-06-19T13:10:58Z","user_mtime":"2015-06-19T13:10:58Z","portion":"part","extent_type":"gigabytes","jsonmodel_type":"extent"}],"dates":[{"lock_version":0,"expression":"Hey look a date","begin":"2014","end":"2015","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-19T13:10:58Z","system_mtime":"2015-06-19T13:10:58Z","user_mtime":"2015-06-19T13:10:58Z","date_type":"inclusive","label":"creation","jsonmodel_type":"date"}],"external_documents":[],"rights_statements":[],"deaccessions":[{"lock_version":0,"description":"Description unavailable.","disposition":"Returned to donor.","notification":false,"created_by":"admin","last_modified_by":"admin","create_time":"2015-06-19T13:10:58Z","system_mtime":"2015-06-19T13:10:58Z","user_mtime":"2015-06-19T13:10:58Z","scope":"whole","jsonmodel_type":"deaccession","extents":[],"repository":{"ref":"/repositories/2"},"date":{"lock_version":0,"expression":"Date unknown","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-19T13:10:58Z","system_mtime":"2015-06-19T13:10:58Z","user_mtime":"2015-06-19T13:10:58Z","date_type":"single","label":"deaccession","jsonmodel_type":"date"}}],"related_resources":[],"linked_agents":[{"role":"source","relator":"dnr","terms":[],"ref":"/agents/people/3"}],"instances":[],"uri":"/repositories/2/accessions/1","repository":{"ref":"/repositories/2"},"collection_management":{"lock_version":0,"processors":"Dallas Pillen","rights_determined":false,"created_by":"admin","last_modified_by":"admin","create_time":"2015-06-19T13:10:58Z","system_mtime":"2015-06-19T13:10:58Z","user_mtime":"2015-06-19T13:10:58Z","processing_priority":"high","processing_status":"in_progress","jsonmodel_type":"collection_management","external_ids":[],"uri":"/repositories/2/collection_management/7","repository":{"ref":"/repositories/2"}}}
+
+"""
+
+"""
+<---This accession has a linked event--->
+{"lock_version":4,"suppressed":false,"title":"this is the title","display_string":"this is the title","publish":true,"content_description":"This is a content description","condition_description":"This is a condition description","disposition":"THis is the disposition","inventory":"this is the inventory\r\n","provenance":"this is the provenance\r\n","general_note":"this is a general note","accession_date":"2015-06-18","restrictions_apply":true,"retention_rule":"this is the retention rule","access_restrictions":true,"access_restrictions_note":"this is the accessrestriction note","use_restrictions":true,"use_restrictions_note":"this is the use restriction note","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-18T13:21:07Z","system_mtime":"2015-06-18T15:24:26Z","user_mtime":"2015-06-18T15:24:24Z","id_0":"123456","acquisition_type":"deposit","resource_type":"papers","jsonmodel_type":"accession","external_ids":[],"related_accessions":[],"subjects":[{"ref":"/subjects/5"},{"ref":"/subjects/14"},{"ref":"/subjects/1"}],"linked_events":[{"ref":"/repositories/2/events/17"}],"extents":[{"lock_version":0,"number":"5","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-18T15:24:25Z","system_mtime":"2015-06-18T15:24:25Z","user_mtime":"2015-06-18T15:24:25Z","portion":"part","extent_type":"linear_feet","jsonmodel_type":"extent"},{"lock_version":0,"number":"100","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-18T15:24:25Z","system_mtime":"2015-06-18T15:24:25Z","user_mtime":"2015-06-18T15:24:25Z","portion":"part","extent_type":"gigabytes","jsonmodel_type":"extent"}],"dates":[{"lock_version":0,"expression":"Hey look a date","begin":"2014","end":"2015","created_by":"admin","last_modified_by":"admin","create_time":"2015-06-18T15:24:25Z","system_mtime":"2015-06-18T15:24:25Z","user_mtime":"2015-06-18T15:24:25Z","date_type":"inclusive","label":"creation","jsonmodel_type":"date"}],"external_documents":[],"rights_statements":[],"deaccessions":[],"related_resources":[],"linked_agents":[{"role":"creator","terms":[],"ref":"/agents/people/3"}],"instances":[],"uri":"/repositories/2/accessions/1","repository":{"ref":"/repositories/2"},"collection_management":{"lock_version":0,"processors":"Dallas Pillen","rights_determined":false,"created_by":"admin","last_modified_by":"admin","create_time":"2015-06-18T15:24:25Z","system_mtime":"2015-06-18T15:24:25Z","user_mtime":"2015-06-18T15:24:25Z","processing_priority":"high","processing_status":"in_progress","jsonmodel_type":"collection_management","external_ids":[],"uri":"/repositories/2/collection_management/2","repository":{"ref":"/repositories/2"}}}
+"""
+
+"""
+<---This is an event--->
+{"lock_version":0,"suppressed":false,"created_by":"admin","last_modified_by":"ad                min","create_time":"2015-06-18T15:23:24Z","system_mtime":"2015-06-18T15:24:26Z",                "user_mtime":"2015-06-18T15:23:24Z","event_type":"agreement_received","jsonmodel                _type":"event","external_ids":[],"linked_agents":[{"role":"recipient","ref":"/ag                ents/people/4"}],"linked_records":[{"role":"source","ref":"/repositories/2/acces                sions/1"}],"uri":"/repositories/2/events/17","repository":{"ref":"/repositories/                2"},"date":{"lock_version":0,"begin":"1999","created_by":"admin","last_modified_                by":"admin","create_time":"2015-06-18T15:23:25Z","system_mtime":"2015-06-18T15:2                3:25Z","user_mtime":"2015-06-18T15:23:25Z","date_type":"single","label":"agent_r                elation","jsonmodel_type":"date"}}
+"""
+
+"""
+<--This is an example of a posted accession-->
+curl -H "X-ArchivesSpace-Session:$TOKEN" -d '{"id_0":"987","accession_date":"1999-09-09"}' http://localhost:8089/repositories/2/accessions
+{"status":"Created","id":2,"lock_version":0,"stale":true,"uri":"/repositories/2/accessions/2","warnings":[]}
+"""
+
+"""
+<---This is a posted event--->
+curl -H "X-ArchivesSpace-Session:$TOKEN" -d '{"event_type":"agreement_received","linked_agents":[{"role":"executing_program","ref":"/agents/software/1"}], "linked_records":[{"role":"source","ref":"/repositories/2/accessions/2"}], "date":{"begin":"1990","date_type":"single","label":"other"}}' http://localhost:8089/repositories/2/events"}}' http://localhost:8089/repos              {"status":"Created","id":18,"lock_version":0,"stale":null,"uri":"/repositories/2/events/18","warnings":[]}
+"""
+
+import json
+import csv
+import requests
+import os
+from os.path import join
+
+
+baseURL = 'http://localhost:8089'
+user='admin'
+password='admin'
+
+auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session':session}
+
+with open('accession_single.csv','rb') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader,None)
+    for row in reader:
+        id_0 = row[1]
+        accession_date = row[5]
+        accession_string = {"id_0":id_0,"accession_date":accession_date}
+        data = json.dumps(accession_string)
+        accession = requests.post(baseURL+'/repositories/2/accessions', headers=headers, data=data).json()
+        print accession
