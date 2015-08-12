4e47bc8c2c2210a26870504755a171b33f5d1717
djpillen@umich.edu <djpillen@umich.edu>
2015-08-12 16:22:31 -0400

    commenting

--- update_archival_object.py
+++ update_archival_object.py
@@ -4,7 +4,7 @@ import csv
 
 # This script will create a new digital object and link it as an instance to an existing archival object
 # This was written under the assumption that you might have a csv (or similar), exported from ASpace or
-# compiled from an ASpace exported EAD, with an existing archival object's refid. Using only the refid,
+# compiled from an ASpace exported EAD, with an existing archival object's ref_id. Using only the ref_id,
 # this will use the ASpace API to search for the existing archival object, retrieve its URI, store the archival
 # object's JSON, create a new digital object using the title from the archival object and an identifier (also from the CSV),
 # grab the URI for the newly created digital object, add the link as an instance to the archival object JSON,
@@ -23,29 +23,31 @@ with open('C:/Users/Public/Documents/digital_object_csv.csv','rb') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
 
-        # We'll use an identifier and a file_uri from the csv to create the digital object
+        # Use an identifier and a file_uri from the csv to create the digital object
+        # If you don't have specific identifiers and just want a random string,
+        # you could do something line 'identifier = uuid.uuid4()'
         identifier = row[3]
         file_uri = row[4]
 
-        # Get the archival object's ASpace ref_id
+        # Grab the archival object's ASpace ref_id from the csv
         ref_id = row[2]
 
-        # Search ASpace for the matching ref_id
+        # Search ASpace for the ref_id
         search = requests.get(aspace_url+'/repositories/2/search?page=1&q='+ref_id,headers=headers).json()
 
         # Grab the archival object uri from the search results
         archival_object_uri = search['results'][0]['uri']
 
-        # Do a get request for the archival object and store the JSON
+        # Submit a get request for the archival object and store the JSON
         archival_object_json = requests.get(aspace_url+archival_object_uri,headers=headers).json()
 
         # Continue only if the search-returned archival object's ref_id matches our starting ref_id
         if archival_object_json['ref_id'] == ref_id:
 
-            # Add the archival object uri to the row in the csv to write it out at the end
+            # Add the archival object uri to the row from the csv to write it out at the end
             row.append(archival_object_uri)
 
-            # Reuse the display string from the archival object as the title
+            # Reuse the display string from the archival object as the digital object title
             # Note: a better way of doing this would be to add the title and dates from the archival object separately
             # This also does not copy over any notes from the archival object
             display_string = archival_object_json['display_string']
@@ -64,7 +66,7 @@ with open('C:/Users/Public/Documents/digital_object_csv.csv','rb') as csvfile:
 
             print 'Digital Object URI', dig_obj_uri
 
-            # Add the digital object uri to the row in the csv to write it out at the end
+            # Add the digital object uri to the row from the csv to write it out at the end
             row.append(dig_obj_uri)
 
             # Build a new instance to add to the archival object, linking to the digital object
