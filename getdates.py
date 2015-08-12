970e4c8c24578c41d88b5f49b0a6a7f472b08f29
djpillen <djpillen@umich.edu>
2015-08-12 16:42:23 -0400

    Update getdates.py

--- getdates.py
+++ getdates.py
@@ -5,14 +5,15 @@ from os.path import join
 import re
 
 
-path = 'C:/Users/djpillen/GitHub/vandura/Real_Masters_all'
+path = 'path/to/EADs'
+
 for filename in os.listdir(path):
     tree = etree.parse(join(path, filename))
     dates = tree.xpath('//unitdate')
     undated = re.compile('^[Uu](ndated)$')
     for date in dates:
         if date.text and not undated.match(date.text) and not 'normal' in date.attrib:
-            with open('C:/Users/Public/Documents/strangedates-73.csv', 'ab') as csvfile:
+            with open('path/to/output.csv', 'ab') as csvfile:
                 writer = csv.writer(csvfile, dialect='excel')
                 writer.writerow([filename, tree.getpath(date), date.text.encode('utf-8')])
     print filename
