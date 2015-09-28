402569acc4190c04e27d96922b2fe6f20a6a0bd2
djpillen@umich.edu <djpillen@umich.edu>
2015-09-28 17:00:09 -0400

    p taggin

--- p_tag_subelements.py
+++ p_tag_subelements.py
@@ -5,12 +5,17 @@ from os.path import join
 path = 'C:/Users/djpillen/GitHub/vandura/Real_Masters_all'
 
 subelements = []
+whatever = ['extptr','num','list','blockquote']
 
 for filename in os.listdir(path):
-    print filename
     tree = etree.parse(join(path,filename))
     for subelement in tree.xpath('//p/*'):
-        if subelement.tag not in subelements:
-            subelements.append(subelement.tag)
+        if subelement.xpath('.//p'):
+            if subelement.tag not in whatever:
+                p_tag = subelement.getparent()
+                print subelement.tag
+                print etree.tostring(p_tag)
+            #if subelement.tag not in subelements:
+                #subelements.append(subelement.tag)
 
-print subelements
+#print subelements
