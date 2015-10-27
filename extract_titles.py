90ed01d565d779ab39988eedda9164e7003897e6
djpillen@umich.edu <djpillen@umich.edu>
2015-10-27 16:56:31 -0400

    remove writing resource tree

--- extract_titles.py
+++ extract_titles.py
@@ -22,8 +22,6 @@ headers = {'X-ArchivesSpace-Session':session}
 # Fetch the resource tree
 resource_tree = requests.get(aspace_url+'/repositories/' + repository + '/resources/' + resource_id +'/tree', headers=headers).json()
 
-with open('C:/Users/Public/Documents/123.json','w') as json_out:
-    json_out.write(json.dumps(resource_tree))
 
 # Function to loop through all children in the resource tree and grab the title
 def find_titles(child,titles):
