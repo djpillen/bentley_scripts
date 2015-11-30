f16754b21453c802d163a5737275cf1573806c52
djpillen@umich.edu <djpillen@umich.edu>
2015-11-30 09:11:52 -0500

    ???

--- classifications.py
+++ classifications.py
@@ -12,10 +12,17 @@ uarp_classification = classification_base + '2'
 for filename in os.listdir(path):
 	print filename
 	tree = etree.parse(join(path,filename))
+	eadheader = tree.xpath('//eadheader')[0]
 	publisher = tree.xpath('//titlepage/publisher')[0]
+	classification_id = False
 	if 'Michigan Historical Collections' in etree.tostring(publisher):
-		publisher.attrib['id'] = mhc_classification
+		classification_id = mhc_classification
 	elif 'University Archives' in etree.tostring(publisher):
-		publisher.attrib['id'] = uarp_classification
+		classification_id = uarp_classification
+	if classification_id:
+		classification = etree.Element('classification')
+		classification.attrib['ref'] = classification_id
+		eadheader.append(classification)
+
 	with open(join(path,filename),'w') as ead_out:
 		ead_out.write(etree.tostring(tree,encoding='utf-8',xml_declaration=True,pretty_print=True))
