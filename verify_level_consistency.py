a641c11149635e05adf8359606d19eeaab5292a9
djpillen@umich.edu <djpillen@umich.edu>
2016-03-22 17:08:56 -0400

    update verify level consistency

--- verify_level_consistency.py
+++ verify_level_consistency.py
@@ -13,18 +13,12 @@ for filename in os.listdir(path):
 	for component in components:
 		subcomponents = component.xpath("./*[starts-with(local-name(), 'c0')]")
 		subcomponent_levels = {}
-		subcomponent_tags = {}
 		for subcomponent in subcomponents:
 			subcomponent_level = subcomponent.attrib["level"]
-			subcomponent_tag = subcomponent.tag
 			subcomponent_levels[subcomponent_level] = subcomponent_levels.get(subcomponent_level, 0) + 1
-			subcomponent_tags[subcomponent_tag] = subcomponent_tags.get(subcomponent_tag, 0) + 1
-		if len(subcomponent_levels) > 1:
+		if len(subcomponent_levels) > 1 and any("series" in level for level in subcomponent_levels):
 			data.append([filename, tree.getpath(component),subcomponent_levels])
 			print filename, tree.getpath(component), subcomponent_levels
-		if len(subcomponent_tags) > 1:
-			data.append([filename, tree.getpath(component), subcomponent_tags])
-			print filename, tree.getpath(component), subcomponent_tags
 
 with open(inconsistency_csv,'wb') as csvfile:
 	writer = csv.writer(csvfile)
